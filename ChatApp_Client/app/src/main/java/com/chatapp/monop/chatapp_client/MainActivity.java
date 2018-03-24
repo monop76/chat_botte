55f0f9ca57ec1cd10fc2f2170668a8c8efe239c4
monop76 <samirhadjoutrios@hotmail.com>
2018-03-25 00:16:40 +0100

    volley post string request

--- ChatApp_Client/app/src/main/java/com/chatapp/monop/chatapp_client/MainActivity.java
+++ ChatApp_Client/app/src/main/java/com/chatapp/monop/chatapp_client/MainActivity.java
@@ -4,7 +4,9 @@ SOURCES
 - tuto openClassrooms : "Développez votre première application android"
 
 A FAIRE
-- associer les champs de saisie au message POST.
+- associer les champs de saisie au message POST
+- modifier l'url de requête quand cela fonctionne
+- nettoyer les imports et le code inutile
 
 ATTENTION
 - Remettre à jour les diagrammes si mon implémentation s'éloigne des modèles :
@@ -12,6 +14,8 @@ ATTENTION
 
 DETTE TECHNIQUE
 - améliorer l'activation du bouton : PSEUDO || CONTACT >> PSEUDO & CONTACT
+- modifier la concaténation de texte : cf les warnings dans le code
+- barre de défilement
 */
 
 package com.chatapp.monop.chatapp_client;
@@ -20,8 +24,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -29,6 +42,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -39,18 +54,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //création des variables et association avec les éléments de vue
-        EditText mPseudo = (EditText)findViewById(R.id.fieldPseudoPerso);
-        EditText mContact = (EditText)findViewById(R.id.fieldPseudoContact);
-        EditText mMessage = (EditText)findViewById(R.id.inputMsg);
-        final Button mPostSend = (Button) findViewById(R.id.btnSend); //"final" pour être accédé depuis l'intérieur d'une classe
+//Variables creations associated to views elements
+        final EditText mPseudo = (EditText) findViewById(R.id.fieldPseudoPerso); //"final" allows a class to call it
+        final EditText mContact = (EditText) findViewById(R.id.fieldPseudoContact);
+        final EditText mMessage = (EditText) findViewById(R.id.inputMsg);
+        final TextView mDiscussion = (TextView) findViewById(R.id.discussion);
+        final Button mPostSend = (Button) findViewById(R.id.btnSend);
 
-        //activation du bouton si saisie dans les champs de pseudo ou de contact
+//Activation of "Envoyer" button if input detected pseudo or contact fields
         mPostSend.setEnabled(false);
 
         mPseudo.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence1, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence charSequence1, int start, int count, int after) {
+            }
 
             @Override
             public void onTextChanged(CharSequence charSequence1, int start, int before, int count) {
@@ -58,12 +75,14 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void afterTextChanged(Editable editable1) {}
+            public void afterTextChanged(Editable editable1) {
+            }
         });
 
         mContact.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence2, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence charSequence2, int start, int count, int after) {
+            }
 
             @Override
             public void onTextChanged(CharSequence charSequence2, int start, int before, int count) {
@@ -71,52 +90,64 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void afterTextChanged(Editable editable2) {}
+            public void afterTextChanged(Editable editable2) {
+            }
         });
 
+//request POST using volley library
 
-        //requête POST
-        try {
-            URL url = new URL("https://www.serverTest.com");
+        //Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
 
-            HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+        //Provide requested URL
+        final String url = "http://www.google.fr";
 
-            String urlParameters = "pseudo="+mPseudo.getText()+"contact="+mContact.getText()+"message="+mMessage.getText();
+        // Request a string response from the provided URL.
+        final StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 1000 characters of the response string.
+                        mDiscussion.setText("Réponse à la requête: " + response.substring(0, 100));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mDiscussion.setText("l'envoi de la requête vers le site : " + url + " a échoué.");
+                    }
+                }){
 
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("USER-AGENT", "Mozilla/5.0"); //à quoi ça sert ?
-            connection.setRequestProperty("ACCEPT-LANGUAGE", "fr-FR"); //à quoi ça sert ?
-            connection.setDoOutput(true);
-/*
-            DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+            //Setting request parameters
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("pseudo", mPseudo.toString());
+                params.put("contact",mContact.toString());
 
-            dStream.writeBytes(urlParameters);
-            dStream.flush();
-            dStream.close();
-
-            int responseCode = connection.getResponseCode();
-            String output = "Request URL " + url;
-            output += System.getProperty("line.separator") + "RequestParameters " + urlParameters;
-            output += System.getProperty("line.separator") + "Response Code " + responseCode;
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            String line = "";
-            StringBuilder responseOutput = new StringBuilder();
-
-            while((line = br.readLine()) != null){
-                responseOutput.append(line);
+                return params;
             }
-            br.close();
+        };
 
-            output += System.getProperty("line.separator") + responseOutput.toString();
+        mPostSend.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                // Add the request to the RequestQueue. At this step, the request is sent
+                queue.add(stringRequest);
+            }
+        });
 
-            mPostSend.setText(output);
-            */
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+/*
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+*/
     }
 }
